769bd458d0b064f98a6e7162ce570d2b0f3dbd4c
aetos <aetos382@users.noreply.github.com>
2024-01-14 12:32:28 +0900

    add simple visualizer

---
+++ VisualizerExtension/SimpleVisualizerProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.DebuggerVisualizers;
+using Microsoft.VisualStudio.RpcContracts.RemoteUI;
+
+namespace VisualizerExtension;
+
+[VisualStudioContribution]
+public sealed class SimpleVisualizerProvider :
+    DebuggerVisualizerProvider
+{
+    public override Task<IRemoteUserControl> CreateVisualizerAsync(
+        VisualizerTarget visualizerTarget,
+        CancellationToken cancellationToken)
+    {
+        return Task.FromResult<IRemoteUserControl>(new VisualizerDialog());
+    }
+
+    public override DebuggerVisualizerProviderConfiguration DebuggerVisualizerProviderConfiguration { get; } =
+        new("%SimpleVisualizer.Name%", typeof(string));
+}
